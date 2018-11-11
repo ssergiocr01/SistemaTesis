243d27fe105394c5ce0368edfc15a5bf16a4b343
Sergio Serrano Vargas <ssergiocr01@gmail.com>
2018-11-11 12:01:36 -0600

    Inicio del Proyecto

---
+++ SistemaTesis/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SistemaTesis.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
